2dc71dcd9b80fd51495673f1ed41eae0f301cd2f
Francisco Mejia <francis_heavy@hotmail.com>
2017-08-12 15:20:57 -0500

    Servicio creado,header y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//rutas
+import { app_routing } from './app.routes';
+
+//services
+import { InformacionService } from './services/informacion.service';
+
+//componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +23,18 @@ import { AboutComponent } from './components/about/about.component';
     HeaderComponent,
     FooterComponent,
     PortafolioComponent,
-    AboutComponent
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    app_routing
   ],
-  providers: [],
+  providers: [
+    InformacionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+//import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import{
+	AboutComponent,
+	PortafolioComponent,
+	ItemComponent
+} from './components/index.paginas';
+
+const app_routes: Routes = [  
+  { path: 'home', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent },
+  { path: '**', pathMatch: 'full', redirectTo:'home' }
+];
+
+//export const appRoutingProviders:any[]=[];
+export const app_routing = RouterModule.forRoot(app_routes,{useHash:true});
