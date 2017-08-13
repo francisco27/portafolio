57a269126e5c00f7e43bb53338483b86d6e809df
Francisco Mejia <francis_heavy@hotmail.com>
2017-08-12 19:27:25 -0500

    Servicio de firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,14 +6,30 @@ export class InformacionService {
 
 	info:any={};
 	cargada:boolean=false;
+  cargada_nosotros:boolean=false;
+  equipo:any[]=[];
 
   constructor( public http:Http) { 
-  	this.http.get('assets/data/info.pagina.json')
-  		.subscribe(data=>{
-  			console.log(data.json());
-  			this.cargada=true;
-  			this.info=data.json();
-  			})
+  	this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info(){
+    this.http.get('assets/data/info.pagina.json')
+      .subscribe(data=>{
+        //console.log(data.json());
+        this.cargada=true;
+        this.info=data.json();
+        });
+  }
+
+  public carga_sobre_nosotros(){
+    this.http.get('https://angularweb-25598.firebaseio.com/equipo.json')
+      .subscribe(data=>{
+        console.log(data.json());
+        this.cargada_nosotros=true;
+        this.equipo=data.json();
+        });
   }
 
 }
